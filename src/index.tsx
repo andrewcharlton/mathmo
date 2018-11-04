d98bd151640514524bf548081cfd16e46532b1f1
Andrew Charlton <a.charlton32@gmail.com>
2018-11-04 18:15:36 +0000

    Add enzyme

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <>
     <h1>MathMo</h1>
   </>,
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
